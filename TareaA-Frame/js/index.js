24fac154be0c792ecd103cc5be7404d98a1eec8a
Jonathan Rodriguez <jonadrod999@hotmail.com>
2021-05-19 20:07:15 -0500

    Tarea de AFrame

---
+++ TareaA-Frame/js/index.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('cursor-listener', {
+    init: function () {
+      var lastIndex = -1;
+      var COLORS = ['red', 'green', 'blue'];
+      this.el.addEventListener('click', function (evt) {
+        src
+      });
+    }
+  });
